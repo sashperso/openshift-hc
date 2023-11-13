ae0811afd034098a755a74026e122a5dc7ff1d23
Anojni Skanthan <askantha@redhat.com>
2023-11-14 02:59:55 +1100

    anju's side of the final mvp iteration 1

--- roles/kubelets/tasks/kubelet.sh
+++ roles/kubelets/tasks/kubelet.sh
@@ -2,8 +2,9 @@
 # For loop to get images from worker and infrastructure nodes
 
 
-for node in $(oc get nodes | awk '{print $1}' | sed '1d')
+for node in $(oc get nodes -l 'node-role.kubernetes.io/control-plane' -o jsonpath='{range .items[*].metadata}{""}{.name}{"\n"}{end}')
 do
-  echo "The node is $node" 
+  echo "The node is $node"
   oc debug node/$node -- chroot /host cat /etc/kubernetes/kubelet.conf
+  
 done
