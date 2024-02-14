321461d8060bc1d2381fae18b9518fe32530f98d
apollo <apollo.bradshaw@gmail.com>
2024-02-14 16:56:32 +1100

    bug fix, noderesource role now working

--- roles/noderesources/filter_plugins/process_resource_output.py
+++ roles/noderesources/filter_plugins/process_resource_output.py
@@ -3,9 +3,17 @@ class FilterModule(object):
     def filters(self):
         return {
             'resource_output_filter': self.resource_output_filter,
+            'resource_overcommit_filter': self.resource_overcommit_filter,
         }
 
     def resource_output_filter(self, resource_str):
         l = str.split(resource_str)
         resource_dict = {"resource_type": l[0], "requests": l[1]+" "+l[2], "limits": l[3]+" "+l[4]}
         return resource_dict
+    
+    def resource_overcommit_filter(self, node_resources):
+        overcommit_list = []
+        for i in node_resources:
+            if int(i["limits"].split('(')[1].split('%')[0]) >= 90:
+                overcommit_list.append(i["resource_type"])   
+        return overcommit_list
